570fb69d322482dcf49d45d6b79dccbeb560632b
staffeev <st.affeev@yandex.ru>
2023-11-17 22:18:42 +0300

    data added

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@ from slovnet.model.emb import NavecEmbedding
 path = 'navec_hudlit_v1_12B_500K_300d_100q.tar'  # 51MB
 navec = Navec.load(path)  # ~1 sec, ~100MB RAM
 
-words = ['аэрация', ]
+words = ['блять', "блядь"]
 ids = [navec.vocab[e] for e in words]
 resume1 = ""
 emb = NavecEmbedding(navec)
