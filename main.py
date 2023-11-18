f0663ada3830f8ab10acb1bb3069355af12e7a69
staffeev <st.affeev@yandex.ru>
2023-11-18 14:11:42 +0300

    agwiyFYGA

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ parser.add_argument("--show_wordcloud", choices=FIELDNAMES, type=str)
 
 
 if __name__ == "__main__":
+
     args = parser.parse_args()
     if args.create_dataset:
         create_dataset(args.folder_path)
